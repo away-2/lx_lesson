06546bd35679c20756016fbd419b927c9e0caf2e
away-2@1968642178@qq.com <1968642178@qq.com>
2023-06-30 12:10:58 +0800

    cannonjs碰撞

--- threejs/three-base/src/App.vue
+++ threejs/three-base/src/App.vue
@@ -4,7 +4,8 @@
   <!-- <Material /> -->
   <!-- <Shadow /> -->
   <!-- <Texture /> -->
-  <Light />
+  <!-- <Light /> -->
+  <Hit /> 
 </template>
 
 <script setup>
@@ -13,7 +14,8 @@
 // import  Material  from './components/03_material/index.vue'
 // import Shadow from './components/03_material/shadow.vue'
 // import Texture from './components/04_texture/index.vue'
-import Light from './components/05_light/index4.vue'
+// import Light from './components/05_light/index4.vue'
+import Hit from './components/06_碰撞/index.vue'
 </script>
 
 <style lang="css" scoped>
