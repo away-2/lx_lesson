12921767ae03f120459b8e45e47332426dddf57a
away-2@1968642178@qq.com <1968642178@qq.com>
2023-07-11 12:01:31 +0800

    动态路由yyds

--- node/node-advanced/08_router/index.js
+++ node/node-advanced/08_router/index.js
@@ -5,13 +5,23 @@ const app = new Server()
 const router = new Router()
 
 // 添加路由
-
+app.use(router.get('/test/:course/:lecture', async({route, res}, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.body = route
+  await next()
+}))
+// 默认路由
+app.use(router.all('.*', async({req, res}, next) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.body = '<h1>Hello World</h1>'
+    await next()
+}))
 
 // 添加拦截切面
-app.use(async ({res}, next) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.body = '<h1>Hello World</h1>'
-  await next()
-})
+// app.use(async ({res}, next) => {
+//   res.setHeader('Content-Type', 'text/html')
+//   res.body = '<h1>Hello World</h1>'
+//   await next()
+// })
 
 app.listen({port: 3000, host: 'localhost'})
