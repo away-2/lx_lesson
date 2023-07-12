2425584bbd3017fbc2680fa42feee826014ee0cd
away-2@1968642178@qq.com <1968642178@qq.com>
2023-07-12 17:12:56 +0800

    学习uniapp

---
+++ uniapp/test/pages/detail/detail.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="detail">
+		<text>detail</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad(option) {
+			console.log(option);
+			let requestTask = uni.request({
+				url: 'https://www.fastmock.site/mock/39ac87de3060aa2bb2ba20a0ff375c81/cat-movie/mostLike',
+				data: {},
+				header: {
+					'custom-header': 'hello'
+				},
+				success(res) {
+					console.log(res);
+					uni.setStorage({
+						key: 'movie_list',
+						data: res.data.movieList,
+						success() {
+							console.log('保存好了');
+						}
+					})
+				}
+			})
+			// requestTask.abort()
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	$red: red;
+
+	.detail {
+		color: green;
+		// #ifndef H5
+		color: $uni-color-error;
+		// #endif
+		font-size: 30rpx;
+	}
+</style>
---
+++ uniapp/test/pages/login/login.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		登录状态：{{$store.state.isLogin}}
+		<button @click="login">登录</button>
+		登录状态：{{isLogin}}
+	</view>
+</template>
+
+<script>
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex'
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		// computed: mapState({
+		// 	isLogin: state => state.isLogin
+		// }),
+		computed: {
+			...mapState(['isLogin'])
+		},
+		methods: {
+			...mapMutations(['storeLogin']),
+			login() {
+				setTimeout(() => {
+					// this.$store.commit('storeLogin')
+					this.storeLogin()
+				}, 1000)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
