86072a573d86c261d416cb95c406455f58776c03
away-2@1968642178@qq.com <1968642178@qq.com>
2023-09-01 23:44:52 +0800

    自测

---
+++ test/1.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+  return nums.length
+};
+
+console.log(removeElement([1,2,3,2,1],2));
