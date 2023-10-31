5ec3998cc7e94bf921c1212d799c24e5f361527e
away-2@1968642178@qq.com <1968642178@qq.com>
2023-10-31 15:28:47 +0800

    redux基础

---
+++ react/redux/react-redux-pro/src/App.js
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+/// 导入actionCreater
+import { inscrement, decrement, addToNum } from "./store/modules/counterStore";
+import { useEffect } from "react";
+import { fetchChannlList } from "./store/modules/channelStore";
+function App() {
+  const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channel)
+  const dispatch = useDispatch()
+  // 使用useEffect触发异步请求执行
+  useEffect(() => {
+    dispatch(fetchChannlList())
+  }, [dispatch])
+  return (
+    <div className="App">
+      <button onClick={() => dispatch(decrement())}>-</button>
+    {count}
+    <button onClick={() => dispatch(inscrement())}>+</button>
+    <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
+    <button onClick={() => dispatch(addToNum(20))}>add to 20</button>
+    <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
