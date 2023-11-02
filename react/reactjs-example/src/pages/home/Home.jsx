18f46bb46fde442a38b832d7bec65bd0ad621c8d
away-2@1968642178@qq.com <1968642178@qq.com>
2023-11-02 17:59:54 +0800

    react项目实战

--- react/reactjs-example/src/pages/home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "antd";
-import React, { useContext, useEffect, useRef } from "react";
-export default function home() {
-  return (
-  <div>
-    这是home页
-  </div>
-  )
-}
--- react/reactjs-example/src/pages/two/Two.jsx
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-
-export default function Two() {
-    return (
-        <div>
-            this is two page
-        </div>
-    )
-}
