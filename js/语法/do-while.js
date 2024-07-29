bd97f65a7958f08d4da8a6425f593a347af771d5
away-2 <1968642178@qq.com>
2024-07-29 22:21:21 +0800

    语法

---
+++ js/语法/do-while.js
@@ -0,0 +1,15 @@
+// do-while
+var i = 0;
+do{
+	i += 2;
+} while(i<10);
+// alert(i);
+console.log(i);
+
+
+// while
+var j = 0;
+ while(j<10){
+	j += 2;
+}
+console.log(j);
---
+++ js/语法/switch.js
@@ -0,0 +1,23 @@
+if (i == 1) {
+    alert("1");
+} else if (i == 2) {
+    alert("2");
+} else if (i == 3) {
+    alert("3");
+} else {
+    alert("4");
+}
+
+switch (i) {
+    case 1:
+        alert("1");
+        break;
+    case 2:
+        alert("2");
+        break;
+    case 3:
+        alert("3");
+        break;
+    default:
+        alert("4");
+}
